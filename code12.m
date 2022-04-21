331b4557d0b174943f7692ff36aee13237b7e883
sdh0572 <sdh0572@gmail.com>
2022-04-21 17:33:38 +0900

    first commit

---
+++ code12.m
@@ -0,0 +1,6 @@
+krw = input(''); %한국 원화 입력
+
+usd = floor(krw/1235); %미국 달러로 환전(소수점X)
+eur = floor(krw/1337); %유럽 유로로 환전(소수점X)
+cny = floor(krw/192); %중국 위안으로 환전(소수점X)
+jpy = floor(krw/9); %일본 엔으로 환전(소수점X)
